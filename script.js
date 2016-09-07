b6b6c0aeab7f9a95d3f628050aac95517e741073
Derek Styer <dmstyer@gmail.com>
2016-09-06 23:38:31 -0600

    redid the first excercise, hopefully this proves I can do the rest of the exercise

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Your JS goes here
+
+
+for (var i = 0; i < 100; i++) {
+  $('body').append(`<div class="first"></div>`);
+  $('body').append(`<div class="second"></div>`);
+  $('.first').css('background-color','red');
+  $('.second').css('background-color','black');
+  $('.first').css('float','left').css('paddingBottom','11.1%').css('width','11.1%');
+  $('.second').css('width','11.1%').css('float','left').css('paddingBottom','11.1%');
+}
